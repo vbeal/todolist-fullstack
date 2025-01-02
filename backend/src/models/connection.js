56bad773d8b96dbbd20504df55d1d8e7378d3b8d
VBeal <victorbeal@gmail.com>
2025-01-02 10:40:06 -0300

    atualizando

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.MYSLQ_HOST,
-  user: process.env.MYSLQ_USER,
-  password: process.env.MYSLQ_PASSWORD,
-  database: process.env.MYSLQ_DB,
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 module.exports = connection;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3307;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running or port ${PORT}`));
