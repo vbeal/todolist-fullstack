f2ae37176a200a868677a337272004814f2d5b6e
VBeal <victorbeal@gmail.com>
2025-01-02 12:42:48 -0300

    at server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3307;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server running or port ${PORT}`));
